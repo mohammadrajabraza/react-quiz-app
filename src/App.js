bd869d182911480697fb73747e90d02cb1cfdeb7
Muhammad Rajab Raza <mohammadrajabraza@gmail.com>
2021-06-22 14:17:27 +0500

    added question component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import Question from './components/Question'
+import {useState} from 'react'
 
 function App() {
+
+
+    const question_bank = [ 
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"Who created the digital distribution platform Steam?","correct_answer":"Valve","incorrect_answers":["Pixeltail Games","Ubisoft","Electronic Arts"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"Who is the main character in &quot;The Stanley Parable&quot;?","correct_answer":"Stanley","incorrect_answers":["The Adventure Line","The Narrator","The Boss"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"In the 2002 video game &quot;Kingdom Hearts&quot;, how many Keyblades are usable?","correct_answer":"18","incorrect_answers":["13","16","15"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"Which of these is NOT the name of a rival gang in the video game Saint&#039;s Row 2?","correct_answer":"The Zin Empire","incorrect_answers":["The Brotherhood","The Ronin","The Sons of Samedi"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"Who is the creator of the Super Smash Bros. Series?","correct_answer":"Masahiro Sakurai","incorrect_answers":["Reggie Fils-Aim&eacute;","Bill Trinen","Hideo Kojima"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"TF2: What code does Soldier put into the door keypad in &quot;Meet the Spy&quot;?","correct_answer":"1111","incorrect_answers":["1432","1337","No code"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"In the Half-Life series, Gordon Freeman&#039;s signature weapon is a:","correct_answer":"Crowbar","incorrect_answers":["Sledgehammer","Fiber Wire","Katana"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"In Minecraft, which two items must be combined to craft a torch?","correct_answer":"Stick and Coal","incorrect_answers":["Stick and Fire","Wood and Coal","Wood and Fire"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"Lanky, Funky, and Chunky are all characters featured in which series owned by Nintendo?","correct_answer":"Donkey Kong","incorrect_answers":["Mario","Kirby","Zelda"]},
+                       {"category":"Entertainment: Video Games","type":"multiple","difficulty":"easy","question":"In the &quot;Metal Gear Solid&quot; series, what&#039;s the name of Solid Snake&#039;s brother?","correct_answer":"Liquid Snake","incorrect_answers":["Kulus Snake","Billy Snake","Gilur Snake"]}
+                      ]
+    const [result, setResult] = useState({current_index: 0, right_answers: 0})
   return (
     <div className="App">
       <section className="container">
@@ -14,13 +31,7 @@ function App() {
                   <p></p>
                 </div>
               </header>
-              <h2 className="titleContainer">The href</h2>
-              <div className="optionContainer">
-                <div className="option">a. Option 1</div>
-                <div className="option">b. Option 2</div>
-                <div className="option">c. Option 3</div>
-                <div className="option">d. Option 4</div>
-              </div>
+              <Question question={question_bank[result.current_index]}/>
               <footer className="questionFooter">
                 <nav className="pagination">
                   <button className="button">Back</button>
---
+++ src/components/Question/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+function Question(props) {
+    // merging correct and incorrect answers into one array and soritng them randomly
+    let answers = [props.question.correct_answer ,...props.question.incorrect_answers]
+        .sort(() => Math.random() - 0.5);
+    return  <div className="mainContainer">
+                <h2 className="titleContainer">{props.question.question}</h2>
+                <div className="optionContainer">
+                    {/* <input type="radio" id="option1"/><label for="option1">a. Option 1</label> */}
+                    {answers.map(ans => <div className="option">{ans}</div>)}
+                </div>
+            </div>
+}
+
+export default Question
